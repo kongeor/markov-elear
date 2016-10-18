9d965472da21e76efabe430b2d24c4f0f3a50ce0
Kostas Georgiadis <kongeor@gmail.com>
2016-10-18 20:44:59 +0300

    Add hints for m1

--- src/markov_elear/generator.clj
+++ src/markov_elear/generator.clj
@@ -1,11 +1,21 @@
 (ns markov-elear.generator)
 
+; clojure.string/split
+
 (defn text->words [text])
+
+; partition-all
 
 (defn words->transitions [words])
 
+; http://clojure.org/guides/destructuring
+
 (defn fmt-trans [trans])
 
+; merge-with, clojure.set/union, reduce
+
 (defn word-chain [word-transitions])
+
+; ->
 
 (defn text->word-chain [text])
